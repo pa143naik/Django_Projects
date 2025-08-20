a902d215c191e3c23c996dbc8ff0a7985ed00522
pa143naik <pavannaik426@gmail.com>
2025-08-20 11:32:34 +0530

    first_commit

--- dproject1/dapp1/models.py
+++ dproject1/dapp1/models.py
@@ -9,13 +9,19 @@ from django.db import models
     
 # class M_model(models.Model):
 #     name=models.CharField(max_length=30)
-#     age=models.IntegerField(default=0)
+#     age=models.IntegerField(default=0) 
 #     location=models.CharField(max_length=100)
 
-# class emp_details(models.Model):
-#     emp_id=models.IntegerField(default=0)
-#     emp_name=models.CharField(max_length=100)
-#     emp_salary=models.IntegerField()
+class emp_details(models.Model):
+    emp_id=models.IntegerField(default=0)
+    emp_name=models.CharField(max_length=100)
+    emp_salary=models.IntegerField()
+
+    def __str__(self):
+        return self.emp_name
+
+    
+
     
 
 
--- dproject1/dproject1/settings.py
+++ dproject1/dproject1/settings.py
@@ -74,19 +74,19 @@ WSGI_APPLICATION = 'dproject1.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME':'sales',
-    #     'USER':'root',
-    #     'PASSWORD':'Pav@485Nav-21',
-    #     'HOST':'127.0.0.1',
-    #     'PORT':'3306'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'sales',
+        'USER':'root',
+        'PASSWORD':'Pav@485Nav-21',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
+    }
 }
 
 
